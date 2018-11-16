ff2ecce8b469e8c50594b187715e9cc72d3283ea
Pete59f1 <43534880+Pete59f1@users.noreply.github.com>
2018-11-16 09:00:36 +0100

    Add files via upload

--- Ex24-DB04 Opg 6.sql
+++ Ex24-DB04 Opg 6.sql
@@ -10,3 +10,8 @@ INSERT INTO BREED VALUES('UNKNOWN', NULL, NULL, NUll);
 ALTER TABLE PET ADD CONSTRAINT PetBreed_FK FOREIGN KEY (PetBreed) REFERENCES BREED(BreedName);
 
 SELECT OwnerLastName, OwnerFirstName, OwnerEmail FROM PET_OWNER WHERE OwnerId IN(SELECT OwnerId FROM PET WHERE PetBreed IN (SELECT BreedName FROM BREED WHERE AverageLifeExpectancy > 15));
+
+SELECT OwnerLastName, OwnerFirstName, OwnerEmail FROM PET_OWNER JOIN PET ON PET_OWNER.OwnerID = PET.OwnerId WHERE PetType LIKE 'Cat';
+SELECT OwnerLastName, OwnerFirstName, OwnerEmail FROM PET_OWNER JOIN PET ON PET_OWNER.OwnerID = PET.OwnerId WHERE PetType = 'Cat' AND PetName = 'Teddy';
+SELECT OwnerLastName, OwnerFirstName, OwnerEmail FROM PET_OWNER AS PO JOIN PET AS P ON PO.OwnerID = P.OwnerId JOIN BREED AS B ON P.PetBreed = B.BreedName WHERE AverageLifeExpectancy > 15;
+SELECT OwnerLastName, OwnerFirstName, PetName, PetType, PetBreed, AverageLifeExpectancy FROM PET_OWNER AS PO JOIN PET AS P ON PO.OwnerID = P.OwnerId JOIN BREED AS B ON P.PetBreed = B.BreedName WHERE PetBreed != 'UNKNOWN';
